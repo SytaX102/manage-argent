35fb08c98724f6db41e50a62c40408474463cbf3
SytaX102 <nathanielbrassard2011@gmail.com>
2026-03-15 19:55:31 -0400

    Refactor calculate function for investment calculation

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function calculate() {
-
-let invest = document.getElementById("invest").value;
-let profit = document.getElementById("profit").value / 100;
-let months = document.getElementById("months").value;
-
-let money = invest;
-
-for (let i = 0; i < months; i++) {
-    money = money + (money * profit);
-}
-
-document.getElementById("result").innerText = "$" + money.toFixed(2);
-
-}
 let chart;
 
 function calculate(){
