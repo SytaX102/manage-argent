8ac59491e49bd951a307fe6c09c72df0485a9af0
SytaX102 <nathanielbrassard2011@gmail.com>
2026-03-15 19:42:32 -0400

    Add calculate function for investment profit calculation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function calculate() {
 
+let invest = document.getElementById("invest").value;
+let profit = document.getElementById("profit").value / 100;
+let months = document.getElementById("months").value;
+
+let money = invest;
+
+for (let i = 0; i < months; i++) {
+    money = money + (money * profit);
+}
+
+document.getElementById("result").innerText = "$" + money.toFixed(2);
+
+}
