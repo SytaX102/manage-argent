c3090b789e1025b2ee1f7a0ba52d758d97a0c4cc
SytaX102 <nathanielbrassard2011@gmail.com>
2026-03-15 19:51:51 -0400

    Implement investment growth calculation and chart
    Added investment calculation and chart creation functionality.

--- script.js
+++ script.js
@@ -13,3 +13,55 @@ for (let i = 0; i < months; i++) {
 document.getElementById("result").innerText = "$" + money.toFixed(2);
 
 }
+let chart;
+
+function calculate(){
+
+let invest = parseFloat(document.getElementById("invest").value);
+let profit = parseFloat(document.getElementById("profit").value)/100;
+let months = parseInt(document.getElementById("months").value);
+
+let money = invest;
+
+let data = [];
+let labels = [];
+
+for(let i=1;i<=months;i++){
+
+money = money + money*profit;
+
+data.push(money.toFixed(2));
+labels.push("Month "+i);
+
+}
+
+document.getElementById("result").innerText="Future: $"+money.toFixed(2);
+
+createChart(labels,data)
+
+}
+
+function createChart(labels,data){
+
+if(chart){
+chart.destroy();
+}
+
+const ctx=document.getElementById("moneyChart");
+
+chart=new Chart(ctx,{
+type:"line",
+data:{
+labels:labels,
+datasets:[{
+label:"Money Growth",
+data:data,
+borderWidth:3
+}]
+},
+options:{
+responsive:true
+}
+});
+
+}
